def1843bab5a516f13dbd03bcbcecc46eb8fb1fa
singlapayal43@gmail.com <singlapayal43@gmail.com>
2020-04-07 06:04:29 +0530

    commited

--- Arithmetic_Operators.py
+++ Arithmetic_Operators.py
@@ -17,3 +17,9 @@
 # Print the three lines as explained above.
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(raw_input())
+b = int(raw_input())
+
+print (a + b)
+print (a - b)
+print (a * b)
