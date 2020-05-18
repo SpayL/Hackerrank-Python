ce5285f8be92ebd4fde36611c645dba7e02958fa
singlapayal43@gmail.com <singlapayal43@gmail.com>
2020-05-18 22:11:56 +0530

    committed

--- name.py
+++ name.py
@@ -21,3 +21,12 @@
 # Explanation 0
 
 # The input read by the program is stored as a string data type. A string is a collection of characters.
+
+#code:
+def print_full_name(a, b):
+    print("Hello" +" " +a +" " +b +"!" +" "+ "You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
