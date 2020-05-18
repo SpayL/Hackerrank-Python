81b55c53a320a18d94e3901f6c12c9c2ca5d02b4
singlapayal43@gmail.com <singlapayal43@gmail.com>
2020-05-18 22:21:45 +0530

    committed

--- mutations.py
+++ mutations.py
@@ -48,3 +48,12 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 # 
 # code:  """
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position + 1:]
+    
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
